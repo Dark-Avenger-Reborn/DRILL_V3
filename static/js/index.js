86d0f8f17bd798c011ade90dd71b3c42a395c72b
Oz Abramovich <184321725+Dark-Avenger-Reborn@users.noreply.github.com>
2024-12-31 12:35:45 -0800

    testing styles?

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,8 @@
 // Initialize the map
 const map = L.map("map").setView([39.8283, -98.5795], 4); // Centered on the USA
 
+alert("{{ variable_name }}")
+
 // Add OpenMapTiles layer
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
