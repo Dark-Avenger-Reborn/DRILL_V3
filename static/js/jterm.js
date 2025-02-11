6ce3708773f925a32dd5c7a30bf4197f9400dcd7
Oz Abramovich <184321725+Dark-Avenger-Reborn@users.noreply.github.com>
2025-02-11 17:48:21 -0500

    added more special keys

--- static/js/jterm.js
+++ static/js/jterm.js
@@ -16,7 +16,13 @@ document.addEventListener("DOMContentLoaded", function () {
   const pageUID = window.location.pathname.split("/")[2];
 
   function isSpecialKey(data) {
-    return ["\x1B[A", "\x1B[B", "\x1B[C", "\x1B[D", "\x03", "\t"].includes(data);
+    return [
+      "\x1B[A", "\x1B[B", "\x1B[C", "\x1B[D",  // Arrow keys
+      "\x1B[1~", "\x1B[4~", "\x1B[5~", "\x1B[6~",  // Home, End, Page Up, Page Down
+      "\x1B[H", "\x1B[F",  // Alternative Home/End keys
+      "\x03", "\x1A",  // Ctrl+C, Ctrl+Z (handled by the shell)
+      "\t"  // Tab key
+    ].includes(data);
   }
 
   // Send raw keystrokes to the server
