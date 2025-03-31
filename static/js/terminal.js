f1af02202a2d6d28f2d7115d81fb7cb220b91a37
Oz Abramovich <184321725+Dark-Avenger-Reborn@users.noreply.github.com>
2025-03-30 23:50:57 -0400

    removed unneeded code

--- static/js/terminal.js
+++ static/js/terminal.js
@@ -9,32 +9,6 @@ if (!show_logout_button) {
   document.querySelector('li > a[href="/logout"]').parentElement.style.display = 'none';
 }
 
-function getCookie(name) {
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) return parts.pop().split(';').shift();
-  return null;
-}
-
-function setCookie(name, value, days = 1) {
-  const d = new Date();
-  d.setTime(d.getTime() + (days * 24 * 60 * 60 * 1000));
-  const expires = "expires=" + d.toUTCString();
-  document.cookie = `${name}=${value}; ${expires}; path=/`;
-}
-
-// Check if the key exists in cookies
-let userKey = getCookie('userKey');
-
-if (!userKey) {
-  // If no key, generate one and set it in cookies
-  userKey = generateRandomKey(32);
-  setCookie('userKey', userKey);
-  console.log('Generated new key:', userKey);
-} else {
-  console.log('Existing key:', userKey);
-}
-
 function ctrl() {
   fetch("/ctrl", {
     method: "POST",
